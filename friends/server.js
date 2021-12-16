4acd7444274bad44907cb57268d3d2e798c475f9
Dylan Cunningham <gdylanc@gmail.com>
2021-12-16 11:57:46 -0700

    wires up gql hello world

--- friends/server.js
+++ friends/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
-
+import {graphqlHTTP} from 'express-graphql';
+import schema from './schema';
+console.log('graogh gtt;', graphqlHTTP)
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('graph server set up')
 });
+
+// simple resolver
+const root = { hello: "this is the response for hello query" }
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, () => console.log(`graph server listening on localhost:${port}/graphql`))
